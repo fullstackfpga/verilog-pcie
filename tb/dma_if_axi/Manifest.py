b9b2bdc4e92658320695fa66e77a13ef025ad91c
fullstackfpga <fullstackfpga@gmail.com>
2023-04-18 17:12:16 -0400

    update sync with hdlmake iverilog

--- tb/dma_if_axi/Manifest.py
+++ tb/dma_if_axi/Manifest.py
@@ -2,7 +2,21 @@ action = "simulation"
 sim_tool = "iverilog"
 sim_top = "dma_if_axi"
 
+iverilog_opt = "-fst"
+vvp_opt = "-test"
+
 #sim_post_cmd = "vsim -do ../vsim.do -c camera_tb"
+
+iverilog_elab_param = {
+    "defparam" : {"AXI_DATA_WIDTH" : 64,
+                   "AXI_ADDR_WIDTH" : 16,
+                   "AXI_STRB_WIDTH" : """AXI_DATA_WIDTH/8""",
+                   "AXI_ID_WIDTH" : 8,
+                   "RAM_SEL_WIDTH" : 2,
+                   "RAM_ADDR_WIDTH" : 16,
+                   "RAM_SEG_COUNT" : 2,
+                   }
+}
 
 files = [
     "../../rtl/dma_if_axi_rd.v",
